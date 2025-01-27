af40843b16c5f08428fe428779e701ac6cd1bde2
JinY0321 <sunplant0321@gmail.com>
2025-01-27 19:17:30 +0900

    이스케이프 시퀀스 코드 추가

---
+++ ConsoleApp1/ConsoleApp1/HelloWorld.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1
+{
+    internal class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My Name is Kero");
+
+            Console.WriteLine(10);
+            Console.WriteLine(3.141592);
+            Console.WriteLine(3 + 3);
+
+            Console.Write("Hello! ");
+            Console.Write("We are Learning ");
+            Console.WriteLine("at TeamSparta");
+
+            //이스케이프 시퀀스
+            Console.WriteLine("Hello\nWorld");
+
+            Console.WriteLine("Name\tAge");
+            Console.WriteLine("Kero\t30");
+            Console.WriteLine("Young\t25");
+
+            Console.WriteLine("We learn \"C# Programming\"");
+
+            Console.WriteLine("He said, \'Hello\' to me.");
+
+            Console.WriteLine("C:\\MyDocuments\\Project\\");
+        }
+    }
+}
