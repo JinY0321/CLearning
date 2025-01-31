de05b06efd7255c09a243db79a6eb02dbc22fbff
JinY0321 <sunplant0321@gmail.com>
2025-01-31 19:12:26 +0900

    2주차 강의 내용 정리-1

---
+++ 1주차 강의 과제/1주차 강의 과제/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _1주차_강의_과제
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("섭씨 온도를 입력하세요: ");
+            //string input = Console.ReadLine();
+
+            //if (double.TryParse(input, out double celsius))
+            //{
+            //    double fahrenheit = (celsius * 9 / 5) + 32;
+            //    Console.WriteLine($"화씨 온도: {fahrenheit:F2}°F");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("유효한 숫자를 입력하세요.");
+            //}
+
+            Console.Write("몸무게(kg)를 입력하세요: ");
+            string weightInput = Console.ReadLine();
+
+            Console.Write("키(m)를 입력하세요: ");
+            string heightInput = Console.ReadLine();
+
+            if (double.TryParse(weightInput, out double weight) && double.TryParse(heightInput, out double height))
+            {
+                double bmi = weight / (height * height);
+                Console.WriteLine($"BMI 지수: {bmi:F2}");
+            }
+            else
+            {
+                Console.WriteLine("유효한 숫자를 입력하세요.");
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.ReadLine을 이용한 입력
+            Console.Write("이름과 나이를 입력하세요. : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", name);
+
+            //다중 입력 기초
+            Console.Write("Enter two numbers: ");
+            string input = Console.ReadLine();    // "10 20"과 같은 문자열을 입력받음
+
+            string[] numbers = input.Split(' ');  // 문자열을 공백으로 구분하여 배열로 만듦
+            int num1 = int.Parse(numbers[0]);     // 첫 번째 값을 정수로 변환하여 저장
+            int num2 = int.Parse(numbers[1]);     // 두 번째 값을 정수로 변환하여 저장
+
+            int sum = num1 + num2;                // 두 수를 더하여 결과를 계산
+
+            Console.WriteLine("The sum of {0} and {1} is {2}.", num1, num2, sum);
+        }
+    }
+}
