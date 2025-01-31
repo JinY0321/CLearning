8bf6e0d176e4f379f28b3ba9598a420f1c4a3c5a
JinY0321 <sunplant0321@gmail.com>
2025-01-31 21:57:22 +0900

    2주차 강의-2 미완.

---
+++ 2주차 강의/2주차 강의-2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _2주차_강의_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1)홀수 짝수 구분하기.
+            Console.Write("번호를 입력하세요. : ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("짝수입니다.");
+            }
+            else
+            {
+                Console.WriteLine("홀수입니다.");
+            }
+
+            //2)등급 출력.
+            int playerScore = 100;
+            string playerRank = "";
+
+            switch
+
+        }
+}
