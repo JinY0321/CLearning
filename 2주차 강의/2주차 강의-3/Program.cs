c2a658c7e264eafe0f39a20f9fd1bcaba75930a6
JinY0321 <sunplant0321@gmail.com>
2025-02-04 20:55:25 +0900

    2주차 강의-3 정리 및 TextRPG 과제 수행 시작

---
+++ 2주차 강의/2주차 강의-3/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _2주차_강의_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////코드 스니펫 for 문 기초
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            ////짝수 출력 하는 예제
+            //for (int i = 0; i < 10; i += 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //코드 스니펫 while문 기초
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            //while 문 예시
+            int count = 0;
+            while (count < 10)
+            {
+                Console.WriteLine("적을 처치했습니다! 남은 적 수: " + (10 - count - 1));
+                count++;
+            }
+
+            Console.WriteLine("축하합니다! 게임에서 승리하셨습니다!");
+
+            //for문 vs while 문
+            //int sum = 0;
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    sum += i;
+            //}
+
+            //Console.WriteLine("1부터 5까지의 합은 " + sum + "입니다.");
+
+            //int sum1 = 0;
+            //int i = 1;
+
+            //while (i <= 5)
+            //{
+            //    sum1 += i;
+            //    i++;
+            //}
+
+            //Console.WriteLine("1부터 5까지의 합은 " + sum1 + "입니다.");
+
+        }
+    }
+}
---
+++ TextRpg/TextRpg/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TextRpg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
+            Console.WriteLine("\n1. 상태보기 \n2. 인벤토리 \n3. 상점");
+            Console.WriteLine("\n원하는 행동을 입력해주세요.\n");
+
+            string mainInput = Console.ReadLine();
+
+            if (mainInput == "1") {
+                //상태보기
+            }
+            else if (mainInput == "2") {
+                //인벤토리
+            }
+            else {
+                //상점열기.
+            }
+        }
+    }
+}
